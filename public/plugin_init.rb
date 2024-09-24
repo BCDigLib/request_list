99fc1c44092593e98b938a21af9206fb0b5b22f9
akofa-a <99671168+akofa-a@users.noreply.github.com>
2024-09-24 10:19:54 -0400

    remove item limit of 20

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -1,5 +1,5 @@
 AppConfig[:request_list] = {
-  :item_limit => 20,
+  #:item_limit => 20,
   :button_position => 0,
   :record_types => ['archival_object', 'resource', 'top_container'],
   :request_handlers => {
